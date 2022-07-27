1014fa9c9f37a2a9a4648621cfe6df98f0d2c860
Ashraf Azemi <ashraf.chzm@gmail.com>
2022-07-27 20:35:01 +0800

    project completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { authActions } from "./store";
+import { useSelector } from "react-redux";
 
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <>
+      <Header />
+
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
+
+      <Counter />
+    </>
   );
 }
 
